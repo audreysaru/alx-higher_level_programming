2067217bcd22dd87bb3a73c86291bd62f1812e95
audreysaru <saruaudrey@gmail.com>
2024-01-14 06:14:01 -0800

    Py hello world

---
+++ 0x00-python-hello_world/index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+sentence = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+
+# Iterate through each character and its index in the sentence
+for index, char in enumerate(sentence):
+    print(f"Character: '{char}', Index: {index}")
