7ddd7617e5eda2e7f91ea89d921b1a11849dfd4a
audreysaru <saruaudrey@gmail.com>
2024-02-24 07:24:53 -0800

    py exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,11 +5,12 @@ def safe_print_list_integers(my_list=[], x=0):
 
     try:
         for value in my_list[:x]:
-            if type(value) == int:
+            if isinstance(value, int):
                 print("{:d}".format(value), end="")
                 count += 1
         print()  # New line after printing integers
-    except (ValueError, TypeError, IndexError):
-        pass
+    except (ValueError, TypeError, IndexError) as e:
+        traceback.print_exc() # pass # Handle the exception or perform additional actions
+        raise e # Re-raise the caught exception to display the traceback
 
     return count
