b0741794b6b3b313c626ed55624303fe517e2d33
audreysaru <saruaudrey@gmail.com>
2024-02-24 05:15:40 -0800

    py data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,8 +6,8 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
     else:
-       num = len(my_list) - 1
+        num = len(my_list) - 1
 
-       while num >= 0:
+        while num >= 0:
             print("{:d}".format(my_list[num]))
             num -= 1
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     # Check if the sentence is empty
     if sentence:
-        # Return a tuple with the length and the first character of the sentence
+        # Return a tuple with length and first character of sentence
         return len(sentence), sentence[0]
     else:
         # If the sentence is empty, return (0, None)
